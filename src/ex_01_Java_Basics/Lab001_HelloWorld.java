8aa51e110b46096612bb0c2f76569448dea45d04
Vani Pulugurtha <pvanikiranmayi29@gmail.com>
2025-12-11 19:43:13 +0530

    changed the syntax of print

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -3,7 +3,7 @@ package ex_01_Java_Basics;
 public class Lab001_HelloWorld {
     public void main()
     {
-        IO.println(String.format("Hello and welcome!"));
+        IO.println("Hello and welcome!");
     }
 
 }
